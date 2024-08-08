56d8b5cc888089b216f0c146a3ab1ba5408cac47
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-08 13:04:23 +0300

    Short-Circuiting And Logical Operators

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -195,3 +195,29 @@ summary;
 const pagesRange = pages > 1000 ? "over a thousand" : "less than 1000";
 pagesRange;
 console.log(`The book has ${pagesRange} pages`);
+
+//Short-Circuiting And Logical Operators: &&, ||, ??
+console.log(true && "Some string");
+console.log(false && "Some string");
+console.log(hasMovieAdaptation && "This book has a movie");
+
+// falsy: 0, '', null, undefined
+console.log("jonas" && "Some string");
+console.log(0 && "Some string");
+
+console.log(true || "Some string");
+console.log(false || "Some string");
+
+console.log(book.translations.spanish);
+
+const spanishTranslation = book.translations.spanish || "NOT TRANSLATED";
+spanishTranslation;
+
+console.log(book.reviews.librarything.reviewsCount);
+const countWrong = book.reviews.librarything.reviewsCount || "no data";
+countWrong;
+
+const count = book.reviews.librarything.reviewsCount ?? "no data";
+count;
+
+//Optional Chaining
