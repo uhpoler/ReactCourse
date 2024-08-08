925056068326b97abb371dbca985c4d377592cfc
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-08 12:49:33 +0300

    Arrow Functions

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -176,11 +176,18 @@ const updatedBook = {
 };
 updatedBook;
 
-//Template Literals
+//Arrow Functions
+// function getYear(str) {
+//   return str.split("-")[0];
+// }
 
-const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${
-  publicationDate.split("-")[0]
-}. The book has ${hasMovieAdaptation ? "" : "not"} been adapted as a movie`;
+const getYear = (str) => str.split("-")[0];
+console.log(getYear(publicationDate));
+
+//Template Literals
+const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${getYear(
+  publicationDate
+)}. The book has ${hasMovieAdaptation ? "" : "not"} been adapted as a movie`;
 
 summary;
 
