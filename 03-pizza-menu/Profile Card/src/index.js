7c577e4d8ff93ecc67632ecef71d4be77efb3927
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-10 14:55:19 +0300

    Added card

--- 03-pizza-menu/Profile Card/src/index.js
+++ 03-pizza-menu/Profile Card/src/index.js
@@ -5,20 +5,58 @@ import "./styles.css";
 function App() {
   return (
     <div className="card">
-      <Avatar />
+      <Avatar photoName="./olga.jpg" />
       <div className="data">
         <Intro />
-        {/* Should contain one Skill component
-        for each web dev skill that you have,
-        customized with props */}
         <SkillList />
       </div>
     </div>
   );
 }
 
-function Avatar() {
-  <img />;
+function Avatar(props) {
+  return (
+    <div>
+      <img className="avatar" src={props.photoName} alt="Olga" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Olga Skrypets</h1>
+      <p>
+        Should contain one Skill component for each web dev skill that you have,
+        customized with propsdkjfvjledfvnlerckwhefufcihwekc wkchkewchkwenc
+        bwdchjkwecn
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML + CSS" color="blue" />
+      <Skill skill="JavaScript" color="yellow" />
+      <Skill skill="Web Design" color="pink" />
+      <Skill skill="Git GitHub" color="red" />
+      <Skill skill="React" color="purple" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill">
+      <span
+        style={{ backgroundColor: props.color, borderRadius: 10, padding: 5 }}
+      >
+        {props.skill}
+      </span>
+    </div>
+  );
 }
 
 const rootElement = document.getElementById("root");
