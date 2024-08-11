a218cce1a491d22752c33542284ce2d75e42c898
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-11 20:06:15 +0300

    initial commit

---
+++ 04-steps/Date Counter/src/App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  return <div>TEST</div>;
+}
