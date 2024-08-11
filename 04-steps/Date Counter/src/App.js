f9ede2ca25f2db81a0da8c3c3c48771e5b81337e
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-11 20:49:47 +0300

    Updated app

--- 04-steps/Date Counter/src/App.js
+++ 04-steps/Date Counter/src/App.js
@@ -10,5 +10,36 @@ export default function App() {
 }
 
 function Counter() {
-  return <div>TEST</div>;
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date("june 11 2024");
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+  );
 }
