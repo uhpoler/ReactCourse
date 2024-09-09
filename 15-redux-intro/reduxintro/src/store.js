63e9031ecd2d270ded88b88789070732dfeff9c0
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-09 13:45:26 +0300

    Worked with action creators

--- 15-redux-intro/reduxintro/src/store.js
+++ 15-redux-intro/reduxintro/src/store.js
@@ -34,18 +34,40 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/deposit", payload: 500 });
 
-console.log(store.getState());
+// console.log(store.getState());
 
-store.dispatch({ type: "account/withdrow", payload: 200 });
-console.log(store.getState());
+// store.dispatch({ type: "account/withdrow", payload: 200 });
+// console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: {
-    amount: 1000,
-    purpose: "buy car",
-  },
-});
-console.log(store.getState());
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: {
+//     amount: 1000,
+//     purpose: "buy car",
+//   },
+// });
+// console.log(store.getState());
+
+// const = ACCOUNT_DEPOSIT
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdrow(amount) {
+  return { type: "account/withdrow", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan(amount) {
+  return { type: "account/payLoan", payload: amount };
+}
+
+store.dispatch(deposit(500));
