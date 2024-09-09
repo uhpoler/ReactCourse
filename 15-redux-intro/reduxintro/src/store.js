4b28bc007f990eb302c9c5e209dbc2df86cbd4e9
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-09 17:36:52 +0300

    Created the account slice

--- 15-redux-intro/reduxintro/src/store.js
+++ 15-redux-intro/reduxintro/src/store.js
@@ -1,17 +1,13 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "@redux-devtools/extension";
+import { configureStore } from "@reduxjs/toolkit";
+
 import accountReducer from "./features/accounts/accountSlice";
 import customerReduser from "./features/customers/customerSlice";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReduser,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReduser,
+  },
 });
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
 export default store;
